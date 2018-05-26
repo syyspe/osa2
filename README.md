5322325f4e80efd0bcab1068b9c000145f714415
Syysmeri, Pekka <pekka.syysmeri@f-secure.com>
2018-05-26 16:24:44 +0300

    siivottu index.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"osa 2 tehtavat" 
+Osan 2 tehtävät
+
+Hakemistossa kokoelmien_renderöinti on vastaus tehtäviin 2.1 ... 2.5
+Hakemistossa puhelinluettelo on vastaus tehtäviin 2.6 ...
--- kokoelmien_renderointi/src/index.js
+++ kokoelmien_renderointi/src/index.js
@@ -3,43 +3,6 @@ import ReactDOM from 'react-dom';
 import App from "./App"
 import './index.css';
 
-
-
-
-
-
-const Yhteensa = (props) => {
-    return (
-        <div>
-            <p>yhteensä {props.kurssi.osat[0].tehtavia 
-                + props.kurssi.osat[1].tehtavia + props.kurssi.osat[2].tehtavia} tehtävää</p>
-        </div>
-    )
-}
-
-/*
-const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-        {
-            nimi: 'Reactin perusteet',
-            tehtavia: 10,
-            id: 1
-        },
-        {
-            nimi: 'Tiedonvälitys propseilla',
-            tehtavia: 7,
-            id: 2
-        },
-        {
-            nimi: 'Komponenttien tila',
-            tehtavia: 14,
-            id: 3
-        }
-    ]
-}
-*/
-
 const kurssit = [
     {
       nimi: 'Half Stack -sovelluskehitys',
